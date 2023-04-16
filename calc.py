4c9ed0b4b0681f3ce2140b425cc0613de0077793
tjsdn <tjsdn1008@naver.com>
2023-04-16 12:40:22 +0900

    complete

--- calc.py
+++ calc.py
@@ -7,12 +7,21 @@ def plus(x, y):
 def minus(x, y):
     return x-y
 
+# return multiplication
+def time(x, y):
+    return x*y
+
+# return division
+def slash(x, y):
+    return x/y
+
+
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus, 2: minus")
+        print("0: exit, 1: plus, 2: minus, 3: time, 4: slash")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -26,7 +35,19 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", minus(x,y))
-        elif check > 2:
+        elif check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", time(x,y))
+        elif check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", slash(x,y))
+        elif check > 4:
             print("Unsupported")
         else:
             print("Thank you")
